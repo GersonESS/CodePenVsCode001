ebe1daf0b4d4ad4cc4e2f1cebcd9ec2086dbc56c
Gerson Santos <gersones@outlook.com>
2021-04-01 18:42:07 -0300

    Inicio
    Como abrir um projeto do CodePen no VSCode

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+var nome = "GersonES"
+var idade = 64;
+idade = idade + 1
+alert(nome + idade);
+var  ValorDolarTxt = prompt("Valor:")
+varlorDularNum = parseInt(ValorDolarTxt)
+ValorReal = varlorDularNum * 5.51
+ValorRealDec = ValorReal.toFixed(2)
+alert(ValorRealDec);
+// Comentaeio
